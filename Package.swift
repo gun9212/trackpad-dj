fc6e1015d98db6c6a3d5d567fdff9b1a320ba4d6
gun9212 <gun9212@naver.com>
2026-03-09 14:11:07 +0900

    feat: Rubber Band scratch engine for vinyl-quality pitch scaling

--- Package.swift
+++ Package.swift
@@ -5,9 +5,17 @@ let package = Package(
     name: "TrackpadDJ",
     platforms: [.macOS(.v13)],
     targets: [
+        .systemLibrary(
+            name: "CRubberBand",
+            path: "Sources/CRubberBand"
+        ),
         .executableTarget(
             name: "TrackpadDJ",
-            path: "Sources/TrackpadDJ"
+            dependencies: ["CRubberBand"],
+            path: "Sources/TrackpadDJ",
+            linkerSettings: [
+                .unsafeFlags(["-L/opt/homebrew/lib"])
+            ]
         )
     ]
 )
